b3cfc145e417cb516822f685f88b8f61fc248db8
karthik <jakustove@gmail.com>
2025-07-15 20:17:23 +0530

    shape is added

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ screen.title("Snake Game")
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 
-b1 = Turtle()
-b1.shape("square")
-b1.color("white")
-b2 = Turtle()
-b2.shape("square")
-b2.color("white")
-b2.goto(x=-20, y=0)
-b3 = Turtle()
-b3.shape("square")
-b3.color("white")
-b3.goto(x=-40, y=0)
+starting_position = [(0,0),(-20,0),(-40,0)]
+for position in starting_position:
+    b1 = Turtle()
+    b1.shape("square")
+    b1.color("white")
+    b1.goto(position)
 
 
 
