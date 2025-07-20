848c9c720f82fe9d8a76cd5d68b74a95930a9184
Karthik Ramesan <136253498+fsydurden@users.noreply.github.com>
2025-07-21 00:00:18 +0530

    add highscore

--- snake.py
+++ snake.py
@@ -33,6 +33,14 @@ class Snake:
         self.add_segment(self.segments[-1].position())
 
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+
 
 
     def move(self):
