03d1f0e264c0fe1e1b4c1192912ab62a86751e3d
Karthik Ramesan <136253498+fsydurden@users.noreply.github.com>
2025-07-21 00:34:18 +0530

    added highscore history scorebord.py

--- scorebord.py
+++ scorebord.py
@@ -8,11 +8,13 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", "r") as file:
+            self.high_score = int(file.read())
+
         self.color("White")
         self.hideturtle()
         self.penup()
-        self.goto(0,270)
+        self.goto(0,260)
         self.update_scoreboard()
 
 
@@ -27,6 +29,8 @@ class Scoreboard(Turtle):
     def restart(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt",mode="w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
